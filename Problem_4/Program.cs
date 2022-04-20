5c88b881a935652fe697a245e32005570666da67
Olga Moran <olga@azimuth.co.za>
2022-04-20 19:27:01 +0800

    Added Problem 4

---
+++ Problem_4/Program.cs
@@ -0,0 +1,16 @@
+﻿// 4. Напишите программу, которая принимает на вход число 
+// и проверяет, кратно ли оно одновременно 7 и 23.
+
+// если вводить рандомное число, то вот так (правда редко можно попасть):
+// int a = new Random().Next(0, 1000); 
+// Console.WriteLine(a);
+
+int a = 160;
+if (a % 7 == 0 & a % 23 == 0)
+{
+    Console.WriteLine(a + " is a multiple of 7 and 23.");
+}
+else
+{
+    Console.WriteLine(a + " is not a multiple of 7 and 23.");
+}
