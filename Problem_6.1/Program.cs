c9613b0a3494f2bc2b73cf27486ac02d2d7a0340
Olga Moran <olga@azimuth.co.za>
2022-04-20 20:11:10 +0800

    Added Problem 6.1

--- Problem_6.1/Program.cs
+++ Problem_6.1/Program.cs
@@ -1,14 +1,11 @@
 ﻿// Напишите метод, который принимает на вход трёхзначное число 
 // и на выходе показывает вторую цифру этого числа.
 
+void SecondDig_3FigNum(int num)
+{
+    num = num / 10 % 10;
+    Console.Write("Second digit of this number is " + num + ".");
+}
 int a = new Random().Next(100, 1000);
 Console.WriteLine(a);
-Console.ReadLine();
-
-void SecondDig_3FigNum(int number)
-{
-    number = number / 10 % 10;
-    Console.Write("Second digit of the number is ");
-    Console.WriteLine(number);
-}
 SecondDig_3FigNum(a);
